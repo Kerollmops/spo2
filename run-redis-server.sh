46a24e2cc007268f1cb260fef5024713964c52e8
Clément Renault <renault.cle@gmail.com>
2019-09-02 22:41:31 +0200

    Async Await system working nicely

--- run-redis-server.sh
+++ run-redis-server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ "$1" == "release" ]; then
+    cargo build --release
     redis-server --loadmodule ./target/release/libspo2.dylib
 else
+    cargo build
     redis-server --loadmodule ./target/debug/libspo2.dylib
 fi
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,25 @@
 #[macro_use] extern crate redismodule;
 
+use std::time::Duration;
+use futures_timer::Interval;
+use futures::prelude::*;
 use redismodule::{parse_integer, Context, RedisError, RedisResult};
 
-fn hello_mul(_: &Context, args: Vec<String>) -> RedisResult {
+fn spawn_future<F>(fut: F)
+where F: Future<Output = ()> + Send + 'static
+{
+    use runtime_raw::Runtime;
+    use runtime_native::Native;
+    Native.spawn_boxed(fut.boxed()).expect("cannot spawn a future");
+}
+
+fn hello_mul(_context: &Context, args: Vec<String>) -> RedisResult {
     if args.len() < 2 {
         return Err(RedisError::WrongArity);
+    }
+
+    for (key, value) in std::env::vars() {
+        println!("{}: {}", key, value);
     }
 
     let nums = args
@@ -14,6 +29,19 @@ fn hello_mul(_: &Context, args: Vec<String>) -> RedisResult {
         .collect::<Result<Vec<i64>, RedisError>>()?;
 
     let product = nums.iter().product();
+
+    spawn_future(async move {
+        let dur = Duration::from_secs(4);
+        let mut stream = Interval::new(dur);
+
+        while let Some(_) = stream.next().await {
+            let url = format!("https://httpbin.org/get?product={}", product);
+            match surf::get(url).recv_string().await {
+                Ok(string) => println!("{}", string),
+                Err(error) => println!("{}", error),
+            }
+        }
+    });
 
     let mut response = Vec::from(nums);
     response.push(product);
