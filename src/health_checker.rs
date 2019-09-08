43d36f0745b2b6e3cbce601854cd7f0534db72fd
Clément Renault <renault.cle@gmail.com>
2019-09-08 13:14:21 +0200

    Basic conditional ping speed up system

--- src/health_checker.rs
+++ src/health_checker.rs
@@ -6,17 +6,37 @@ use futures_timer::{Interval, TryFutureExt};
 use redismodule::ThreadSafeContext;
 use url::Url;
 
-const TIMEOUT: Duration = Duration::from_secs(5);
+const TIMEOUT:      Duration = Duration::from_secs(5);
+const NORMAL_PING:  Duration = Duration::from_secs(3);
+const FAST_PING:    Duration = Duration::from_millis(800);
+const INSTANT_PING: Duration = Duration::from_millis(100);
+
+type ArrayDeque10<T> = arraydeque::ArrayDeque<[T; 10], arraydeque::Wrapping>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Status {
+    Healthy,
+    Unhealthy,
+    Unreacheable,
+}
+
+use Status::{Healthy, Unhealthy, Unreacheable};
+
+impl Status {
+    fn is_good(&self) -> bool {
+        *self == Healthy
+    }
+}
 
 pub async fn health_checker(url: Url) {
     let ctx = ThreadSafeContext::create();
-    let dur = Duration::from_secs(10);
-    let mut stream = Interval::new(dur);
+    let mut last_status = ArrayDeque10::new();
+    let mut stream = Interval::new(INSTANT_PING);
 
     while let Some(_) = stream.next().await {
         let key = ctx.open_key_writable(url.as_str());
 
-        match surf::get(&url).timeout(TIMEOUT).await {
+        let status = match surf::get(&url).timeout(TIMEOUT).await {
             Ok(response) => {
                 let status = response.status();
 
@@ -24,11 +44,31 @@ pub async fn health_checker(url: Url) {
                 if key.is_empty() { break }
                 let _ = key.write(&string);
 
-                if !status.is_success() {
-                    eprintln!("{} {}", url, status);
+                if status.is_success() {
+                    Healthy
+                } else {
+                    Unhealthy
                 }
             },
-            Err(error) => eprintln!("{} {}", url, error),
+            Err(_) => Unreacheable,
+        };
+
+        last_status.push_front(status);
+
+        let len = last_status.len() as f32;
+        let bads = last_status.iter().filter(|s| !s.is_good()).count() as f32;
+        let ratio = bads / len;
+
+        println!("{}/{} = {}", len, bads, ratio);
+
+        // last status is bad or half of the status are bad
+        if !status.is_good() || ratio >= 0.5 {
+            eprintln!("{} speed up ping", url);
+            stream = Interval::new(FAST_PING);
+        } else {
+            stream = Interval::new(NORMAL_PING);
         }
+
+        drop(key);
     }
 }
